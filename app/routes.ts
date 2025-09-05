4cd82cbe79dd2a05db5ce29ea774ff7be3b774d3
Bhargava-Shankar <bhargavashankar2003@gmail.com>
2025-09-06 01:56:02 +0530

    INDEX.HTML

--- app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index,route } from "@react-router/dev/routes";
-
-export default [
-    index("routes/home.tsx"),
-    route("post/one-year","routes/06092025.tsx")
-    
-] satisfies RouteConfig;
